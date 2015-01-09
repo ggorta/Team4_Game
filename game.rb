eb4da23d1aadb83449742cf6484838d83c67dc60
Genevieve Gorta <gengo@Genevieves-MacBook-Pro.local>
2015-01-09 15:22:58 -0500

    baddies

--- game.rb
+++ game.rb
@@ -3,29 +3,59 @@
 
 # Character Objects:
 
-  pokemon = [
-    #Squirtle
-    {
-      name: "Squirtle",
-      attack1: "Tackle",
-      attack2: "Watergun",
-      attack3: "Hydropump",
-      health: 10
-    },
-    #Bulbarsaur
-    {
-      name: "Bulbasaur",
-      attack1: "Tackle",
-      attack2: "Vine Whip",
-      attack3: "Seed Bomb",
-      health: 10
-    },
-    #Charmander
-    {
-      name: "Charmander",
-      attack1: "Scratch",
-      attack2: "Dragon Rage",
-      attack3: "Inferno",
-      health: 10
-    },
-  ]
+pokemon = [
+  #Squirtle
+  {
+    name: "Squirtle",
+    attack1: "Tackle",
+    attack2: "Watergun",
+    attack3: "Hydropump",
+    health: 10
+  },
+  #Bulbarsaur
+  {
+    name: "Bulbasaur",
+    attack1: "Tackle",
+    attack2: "Vine Whip",
+    attack3: "Seed Bomb",
+    health: 10
+  },
+  #Charmander
+  {
+    name: "Charmander",
+    attack1: "Scratch",
+    attack2: "Dragon Rage",
+    attack3: "Inferno",
+    health: 10
+  }
+]
+
+wild_pokemon = [
+  #Zubat
+  {
+    name: "Zubat",
+    attack1: "Mean Look",
+    attack2: "Wing Attack",
+    attack3: "Venoshock",
+    health: 10
+  },
+
+  #Mankey
+  {
+    name: "Mankey",
+    attack1: "Leer",
+    attack2: "Fury Swipes",
+    attack3: "Seismic Toss",
+    health: 10
+  },
+
+  #Jigglypuff
+  {
+    name: "Jigglypuff",
+    attack1: "Mud-Slap",
+    attack2: "Headbutt",
+    attack3: "Dynamic Punch",
+    health: 10
+  }
+
+]
