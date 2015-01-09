5b51d6e3839de0c83b70b5f36ecc059f5c5f59b7
bricea27 <bricea27@gmail.com>
2015-01-09 14:40:43 -0500

    character hashes added

--- game.rb
+++ game.rb
@@ -1,2 +1,34 @@
 # Team 4 the Win
 # GOTTA CATCH EM
+
+# Character Objects:
+
+  pokemon = [
+    #Squirtle
+    {
+    "name" => "Squirtle",
+    "attack1" => "Tackle",
+    "attack2" => "Watergun",
+    "attack3" => "Hydropump",
+    "health" => 10
+    },
+    #Bulbarsaur
+    {
+      "name" => "Bulbarsaur",
+      "attack1" => "Tackle",
+      "attack2" => "Vine Whip",
+      "attack3" => "Seed Bomb",
+      "health" => 10
+    },
+    #Charmander
+    {
+      "name" => "Charmander",
+      "attack1" => "Scratch",
+      "attack2" => "Dragon Rage",
+      "attack3" => "Inferno",
+      "health" => 10
+    },
+  ]
+
+
+puts pokemon
