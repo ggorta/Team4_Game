d02090ce3595d42e2aa68ed02aa9f8f8ad4e287b
Genevieve Gorta <gengo@Genevieves-MacBook-Pro.local>
2015-01-12 16:58:41 -0500

    begin do-over

---
+++ game_gg.rb
@@ -0,0 +1,12 @@
+# Team 4 the Win
+# GOTTA CATCH EM ALL
+
+require "colorize"
+
+def intro
+
+end
+puts intro
+
+puts "Welcome to the game! Please tell us your name, and we'll get you logged into our system."
+player_name = gets.chomp
--- pika.rb
+++ pika.rb
@@ -14,6 +14,7 @@ puts "┊▕┈▔▔┈┈┈▔▔┈▏╱┈╱┊".colorize(:yellow)
 puts "┊▕┈┈┈┈┈┈┈┈▕▔┈╱┊┊".colorize(:yellow)
 puts "┈┈╲┈┈┈┈┈┈┈╱▔▔┈┈┈".colorize(:yellow)
 puts "┈┈▕▂╱▔▔▔╲▂▏┈┈┈┈┈".colorize(:yellow)
+#art from: http://textart4u.blogspot.com/2012/04/pikachu-text-art-ascii-art.html
 end
 
 puts pikapika
